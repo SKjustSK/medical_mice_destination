3308349e7a4861e369dd83fbe06f2290aa58769e
Sarthak Singh Tariyal <sarthaksinghtariyal@gmail.com>
2025-01-13 21:02:11 +0530

    Completed basic layout of Hospital sign up page

--- components/hospital/HospitalSignUpPage.tsx
+++ components/hospital/HospitalSignUpPage.tsx
@@ -1,15 +1,19 @@
+"use client"
+
 import NavBar from "../NavBar"
-import { Card } from "../ui/card"
+import { Card, CardHeader, CardDescription, CardTitle } from "../ui/card"
+import { Progress } from "@/components/ui/progress"
 import Link from "next/link"
+import { useState } from "react"
 
 // basic info and other stuff import
 function InfoCard({ title, stepNumber }) {
   return (
     <Link href="/">
-      <Card className="flex flex-col gap-4 p-6 w-full h-52 justify-center">
+      <Card className="flex flex-col gap-4 p-10 w-full h-40 justify-center">
         <div className="flex gap-8">
           <div className="flex flex-col gap-2">
-            <div className="text-3xl font-bold">{title}</div>
+            <div className="text-2xl font-bold">{title}</div>
             <div className="text-slate-700">Step {stepNumber} of 5</div>
           </div>
         </div>
@@ -40,22 +44,51 @@ const steps = [
 ]
 
 function HospitalSignUp() {
+
+  let [stepCompletion, setstepCompletion] = useState({
+    "basicInfo": true,
+    "mediaUpload": false,
+    "facilityDetails": false,
+    "legalDocuments": false,
+    "verification": false,
+  });
+
+  const checkStepsCompleted = (stepCompletion) => {
+    let stepsCompleted = 0;
+    for (let key in stepCompletion) {
+      if (stepCompletion[key]) {
+        stepsCompleted += 1;
+      }
+    }
+    return stepsCompleted
+  }
+  let stepsCompleted = checkStepsCompleted(stepCompletion)
+  let totalSteps = Object.keys(stepCompletion).length
+  let completionPercent = (stepsCompleted*100)/totalSteps
+
   return (
-    <div className="bg-slate-200 flex-col flex flex-1 p-16 gap-8 justify-center">
+    <div className="bg-slate-200 flex-col flex flex-1 px-36 gap-8 justify-center">
       <div className="flex flex-col gap-2 ">
         <div className="text-5xl font-bold">Welcome to Hospital Registration Portal</div>
         <div className="text-xl">Complete the following steps to register your hospital</div>
       </div>
-      <div className="grid grid-cols-3 gap-12 grid-flow-row">
+      <div className="grid grid-cols-3 gap-8 grid-flow-row">
         {steps.map((step, index) => {
           return (
             <InfoCard key={index} title={step.title} stepNumber={index+1}/>
           )
         })}
       </div>
-      <div>
-        Progress Bar
-      </div>
+      <Card className="p-4">
+        <CardHeader className="text-2xl gap-2">
+          <CardTitle>Registration Progress</CardTitle>
+          <Progress className='[&>*]:bg-blue-500' value={completionPercent}/>
+          <CardDescription className="flex justify-between text-lg">
+            <div>{stepsCompleted} of {totalSteps} completed</div>
+            <div>{completionPercent}% complete</div>
+          </CardDescription>
+        </CardHeader>       
+      </Card>
     </div>
   )
 }
